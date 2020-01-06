925ce66b1d2078ee300c73ae707c36ed26d7e021
ZhenHe17 <geshengming17@gmail.com>
2020-01-06 16:31:07 +0800

    解决ejs处理后的空行

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # Docker Nest Template
 
-<% if (keepReadme) { %>
 A template for [create-docker-nest-app](https://www.npmjs.com/package/create-docker-nest-app)
-
-## Generate your project
-
-``` bash
-npm i create-docker-nest-app -g
-cdna my-app
-```
-<% } %>
 
 ## QuickStart
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
-<% if (MongoDB) { %>
-// mongo mongoose modules and example
+<% if (MongoDB) { %>// mongo mongoose modules and example
 import { CatsModule } from './cats/cats.module';
-<% } %>
-<% if (MySQL) { %>
-// mysql typeorm modules and example
+<% } %><% if (MySQL) { %>// mysql typeorm modules and example
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Photo } from './photo/photo.entity';
 import { PhotoModule } from './photo/photo.module';
-<% } %>
-<% if (redis) { %>
-// redis
+<% } %><% if (redis) { %>// redis
 const redis = require("redis");
 const client = redis.createClient({
   host: 'redis',
@@ -19,12 +14,9 @@ const client = redis.createClient({
 client.set("key", "value");
 client.get("key", function (err, reply) {
     console.log('redis reply', reply);
-});
-<% } %>
-
+});<% } %>
 @Module({
-  imports: [
-    <% if (MySQL) { %>
+  imports: [<% if (MySQL) { %>
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'mysql',
@@ -35,11 +27,8 @@ client.get("key", function (err, reply) {
       entities: [Photo],
       synchronize: true,
     }),
-    PhotoModule,
-    <% } %>
-    <% if (MongoDB) { %>
-    CatsModule,
-    <% } %>
+    PhotoModule,<% } %><% if (MongoDB) { %>
+    CatsModule,<% } %>
   ],
 })
 export class AppModule {}
